46ac8b068855bd6d5972512f241ef9bd21ef427f
Gloriousjoy <gloriousfab99@gmail.com>
2022-09-30 00:33:19 +0300

    completed assignment

--- src/Parent.js
+++ src/Parent.js
@@ -5,13 +5,29 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState('#FFF')
+
+//   function handleChangeColor() {
+//     const newRandomColor = getRandomColor();
+//     const randomColor = getRandomColor();
+//     const [color, setColor] = useState(randomColor);
+//     const [childrenColor, setChildrenColor] = useState("#FFF");
+//   }
+    function handleChangeColor(newChildColor) {
+      const newRandomColor = getRandomColor();
+      setColor(newRandomColor);
+      setChildrenColor(newChildColor);
+    }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      {/* <Child onChangeColor={handleChangeColor} />
+      <Child onChangeColor={handleChangeColor} /> */}
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
+
 
 export default Parent;
